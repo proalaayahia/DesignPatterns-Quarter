f9fa63bab0ff110b9de9c52c10db173e973433be
Alaa Yahia <Alaa.Yahia@linkdev.com>
2024-08-03 13:24:39 +0300

    commit

--- ObserverPattern/Program.cs
+++ ObserverPattern/Program.cs
@@ -1,29 +1,18 @@
 ﻿using ObserverPattern.Core;
 
-
-// Create the store
 Store store = new Store();
 
-// Create customers who are interested in different products
 Customer customer1 = new Customer("Alice", "iPhone");
 Customer customer2 = new Customer("Bob", "Samsung Galaxy");
 
-// Customers subscribe to the store notifications
+
 store.Subscribe(customer1);
 store.Subscribe(customer2);
 
-// Add a product to the store
 store.AddProduct("iPhone");
 
-// add two observers
-orderService.AddObserver(ticketStockService);
-orderService.AddObserver(ticketResellerService);
+store.AddProduct("Samsung Galaxy");
 
-// notify
-orderService.CompleteTicketSale(1, 2);
-
-// Unsubscribe Customer 1
 store.Unsubscribe(customer1);
 
-// Add another product
 store.AddProduct("Google Pixel");
